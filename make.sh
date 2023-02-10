d60bedc47e6f47e90e7ef544a3a927430b8514cc
wsm25 <55098744+wsm25@users.noreply.github.com>
2023-02-10 23:43:02 +0800

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,24 @@
+data_path='../domains/data/'
+cndm_file='./cndm.txt'
+
+# referring to https://github.com/v2fly/domain-list-community
+handle_file(){ # param: file
+    if [ -z $1 ] || [ ! "-a $1" ] ; then return 0; fi; # check file
+    cat ${data_path}/$1 | while read line
+    do
+        line=$(echo $line | sed 's/#.*$//') # clear comment
+        if [ -n $line ] ; then continue; fi; # skip empty lines
+        
+        # include
+        inc=$(echo $line | grep "include:")
+        if [ -n "${inc//include:/}" ] ; then
+            handle_file ${inc//include:/}
+            continue;
+        fi
+        # other
+        if [ -n "$(echo $line | grep "keyword:")" ] ; then continue; fi;
+        # look into issue https://github.com/v2fly/domain-list-community/issues/390
+        echo $line | sed  's/^domain:\|^regexp:\|^full://' | sed 's/@.*$//'>> $cndm_file
+        
+    done
+}
